8e8f25e64bfa02604449f3a7d50028f2ae027c01
Evilcome <evilcome.com@gmail.com>
2015-07-03 12:16:26 +0800

    rm mongoose connect

--- evilcome-com/test-app/index.js
+++ evilcome-com/test-app/index.js
@@ -6,6 +6,7 @@ var app = express();
 
 app.get('/', function(req, res, next) {
   // mongoose.connect('mongodb://localhost/' + process.env.MONGO_NAME);
+  console.log(new Date());
   res.json(process.env);
 });
 
