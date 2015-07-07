e4716a740c3743ce9ceff783b0f0aa60c69a890c
Evilcome <evilcome.com@gmail.com>
2015-07-07 14:51:42 +0800

    test ssl

--- swift-how/app/index.js
+++ swift-how/app/index.js
@@ -1,13 +1,17 @@
 var express = require('express'),
-    http = require('http');
+    https = require('http'),
+    fs = require('fs');
 
 var app = express();
 app.use(express.static(__dirname + '/images'));
+
+var privateKey = fs.readFileSync(process.env.SSL_KEY_PATH);
+var certificate = fs.readFileSync(process.env.SSL_CRT_PATH);
 
 app.get('/', function(req, res, next) {
   res.send('<html><head></head><body style="text-align:center"><img src="holdon.png" /></body></html>');
 });
 
-http.createServer(app).listen(process.env.PORT || 8003, function() {
+https.createServer({ key: privateKey, cert: certificate }, app).listen(process.env.PORT || 8003, function() {
   console.log('Listening on port ' + (process.env.PORT || 8003));
 });
